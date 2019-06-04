f6ef17c2fd2fdd350370b98fc5eb0bb7d2c61053
Vijay Nair <43559556+vjk3@users.noreply.github.com>
2019-06-04 20:10:18 +0530

    Initial commit.

---
+++ lib.js
@@ -0,0 +1,9 @@
+// 1.  Handle incoming GET
+// 1.1 Serve static files
+// 2.  Initiate outgoing GET and fetch data from external APIs
+// 3.  Handle incoming POST
+// 3.1 Handle file uploads
+// 3.2 Handle login forms and User Authentication including OAuth/Passport
+// 4.  Initiate outgoing POST and push data to external APIs
+// 5.  Read data from MongoDB
+// 6.  Push data into MongoDB
